3e4c19217676f89d6bc0d0693b5fd25e9ea43066
Enrique Aguilar <eaguilarg23@gmail.com>
2024-11-13 11:04:04 -0600

    Creación de Iniciar Sesión, faltan ajustar detalles visuales

--- app/index.tsx
+++ app/index.tsx
@@ -1,141 +1,30 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+//importtacion de librerias
+import { Text, StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
+//importacion de ventanas
+import Registro from "../scenes/Registro";
+import IniciarSesion from "../scenes/IniciarSesion";
 
-const RegistrationScreen = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const handleRegistration = () => {
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
+const Stack = createStackNavigator();
 
+export default function index() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Registrate</Text>
-      <Text style={styles.legend}>Date de alta llenando los siguientes datos</Text>
-
-      <Text style={styles.Subtitle}>Nombre completo</Text>
-      <TextInput
-        style={styles.input}
-        value={name}
-        onChangeText={setName}
-        placeholder="Francisco Pérez"
-      />
-
-      <Text style={styles.Subtitle}>Email</Text>
-      <TextInput
-        style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-        placeholder="hey@tueemail.com"
-      />
-
-      <Text style={styles.Subtitle}>Contraseña</Text>
-      <TextInput
-        style={styles.input}
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-        placeholder="Ingresa una contraseña"
-      />
-
-      <TouchableOpacity onPress={handleRegistration} style={styles.buttonContainer}>
-        <LinearGradient
-          colors={['#435058', '#4A5861', '#687C88', '#758C9A', '#91ADBE']}
-          locations={[0, 0.47, 0.69, 0.8, 1]}
-          style={styles.gradientButton}
-        >
-          <Text style={styles.buttonText}>Crear Cuenta</Text>
-        </LinearGradient>
-      </TouchableOpacity>
-
-      <Text style={styles.footer}>¿Ya tienes cuenta? Inicia Sesión</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Registro">
+        <Stack.Screen
+          name="Registro"
+          component={Registro}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="IniciarSesion"
+          component={IniciarSesion}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#848E8C',
-    padding: 20,
-    justifyContent: 'center',
-    paddingLeft: 40,
-    paddingRight: 40,
-
-  },
-
-  Subtitle:{
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-    paddingBottom: 15,
-    paddingLeft: 10,
-    paddingRight: 10,
-
-  },
-  title: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    color: 'white',
-    position: 'absolute',
-    top: 120,
-    left: 50,
-  },
-  legend: {
-    fontSize: 17,
-    fontWeight: '300',
-    marginBottom: 20,
-    color: 'white',
-    position: 'absolute',
-    top: 160,
-    left: 50,
-    
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  input: {
-    height: 40,
-    borderWidth: 2,
-    borderColor: '#DCF763',
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-  },
-  buttonContainer: {
-    position: 'absolute',
-    bottom: 100,
-    left: 40,
-    right: 40,
-    borderRadius: 5,
-    overflow: 'hidden',
-    marginBottom: 15,
-  },
-  gradientButton: {
-    paddingVertical: 10,
-    alignItems: 'center',
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  footer: {
-    position: 'absolute',
-    color: 'white',
-    bottom: 70,
-    left: 40,
-    right: 40,
-    fontSize: 14,
-    textAlign: 'center',
-  },
-});
-
-export default RegistrationScreen;
+}
---
+++ scenes/IniciarSesion.js
@@ -0,0 +1,198 @@
+import {
+  Text,
+  StyleSheet,
+  View,
+  Button,
+  TextInput,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import React, { Component } from "react";
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
+
+const IniciarSesion = () => {
+  const navigation = useNavigation();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.Main}>
+      <Image
+        style={styles.picture}
+        source={{
+          uri: "https://images.unsplash.com/photo-1450101499163-c8848c66ca85?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        }}
+      />
+      <View style={styles.bottomPart}>
+        <View style={styles.titleCont}>
+          <Text style={styles.titleTxt}>Iniciar sesión</Text>
+          <Text style={styles.subtitleTxt}>
+            Accede a la aplicación con tu cuenta
+          </Text>
+        </View>
+        <View style={styles.formCont}>
+          <Text style={styles.titleFormTxt}>Email</Text>
+          <TextInput
+            style={styles.inputForm}
+            value={email}
+            onChangeText={setEmail}
+            placeholder="hey@tueemail.com"
+          />
+          <Text style={styles.titleFormTxt}>Contraseña</Text>
+          <TextInput
+            style={styles.inputForm}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            placeholder="Ingresa una contraseña"
+          />
+        </View>
+        {/* <View
+          style={{
+            alignItems: "center",
+            width: "70%",
+            height: "4%",
+            marginLeft: "14%",
+          }}
+        > */}
+        {/* <LinearGradient
+          colors={["#4A6572", "#647C90"]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 0 }}
+          style={styles.gradientBackground}
+        >
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Registro")}
+            style={styles.btnRegister}
+          >
+            <Text style={styles.btnInicioTxt}>Iniciar sesión</Text>
+          </TouchableOpacity>
+        </LinearGradient> */}
+
+        <TouchableOpacity
+          onPress={navigation.navigate("IniciarSesion")}
+          style={styles.buttonContainer}
+        >
+          <LinearGradient
+            colors={["#435058", "#4A5861", "#687C88", "#758C9A", "#91ADBE"]}
+            locations={[0, 0.47, 0.69, 0.8, 1]}
+            style={styles.gradientButton}
+          >
+            <Text style={styles.btnInicioTxt}>Iniciar Sesión</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+
+        {/* </View> */}
+
+        <View style={styles.footer}>
+          <Text style={styles.footerTxt}>¿Aún no tienes cuenta?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Registro")}>
+            <Text style={[styles.footerTxt, { fontWeight: "bold" }]}>
+              Registrarse
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Main: {
+    flex: 1,
+    //justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#848E8C",
+  },
+  bottomPart: {
+    //backgroundColor: "yellow",
+    width: "90%",
+    height: "100%",
+    marginTop: "-15%",
+  },
+  picture: {
+    resizeMode: "contain",
+    width: 500,
+    height: 500,
+    marginTop: "-25%",
+  },
+  titleCont: {
+    //backgroundColor: "red",
+    gap: 12,
+  },
+  titleTxt: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  subtitleTxt: {
+    color: "white",
+    fontSize: 14,
+  },
+  formCont: {
+    //backgroundColor: "green",
+    marginTop: "4%",
+    height: "30%",
+    gap: 20,
+  },
+  titleFormTxt: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  inputForm: {
+    borderWidth: 4,
+    borderColor: "#DCF763",
+    height: "25%",
+    //backgroundColor: "red",
+    borderRadius: 10,
+    paddingLeft: 12,
+  },
+  btnRegister: {
+    //backgroundColor: "blue",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "70%",
+    height: "4%",
+    marginTop: "8%",
+    borderRadius: 10,
+    borderColor: "black",
+    borderWidth: 1,
+  },
+  btnInicioTxt: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  footer: {
+    //backgroundColor: "red",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "0%",
+    gap: 12,
+  },
+  footerTxt: {
+    color: "white",
+  },
+  gradientButton: {
+    //paddingVertical: 10,
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  buttonContainer: {
+    width: "80%",
+    height: "5%",
+    borderRadius: 5,
+    //overflow: "hidden",
+    marginTop: "8%",
+    marginLeft: "10%",
+    borderWidth: 1,
+    borderColor: "black",
+  },
+});
+
+export default IniciarSesion;
