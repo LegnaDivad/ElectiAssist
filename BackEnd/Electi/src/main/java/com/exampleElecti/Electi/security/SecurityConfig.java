bba25dee86cd29c093f3df90340af5dabf693ceb
juanj <juajocervantes16@gmail.com>
2025-01-22 09:47:28 -0600

    Tokens done, deactivated for testing

--- BackEnd/Electi/src/main/java/com/exampleElecti/Electi/security/SecurityConfig.java
+++ BackEnd/Electi/src/main/java/com/exampleElecti/Electi/security/SecurityConfig.java
@@ -1,14 +1,31 @@
 package com.exampleElecti.Electi.security;
 
 import com.exampleElecti.Electi.service.JwtService;
+import org.apache.catalina.User;
+import org.apache.catalina.filters.ExpiresFilter;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
-    private final JwtService jwtService;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll()
+                ).formLogin(formLogin -> formLogin.disable())
+                .httpBasic(httpBasic -> httpBasic.disable());
 
-    public SecurityConfig(JwtService jwtService) {
-        this.jwtService = jwtService;
+        return http.build();
+
     }
+
 }
